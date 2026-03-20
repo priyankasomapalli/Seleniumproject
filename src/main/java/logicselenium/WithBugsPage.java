23adae16dd9af2b2f8b19bf635f432c99ba029a9
priyankasomapalli <priyanka.mandava1412@gmail.com>
2026-03-20 10:39:36 -0400

    Commiting the assignment using and without using faker

--- src/main/java/logicselenium/WithBugsPage.java
+++ src/main/java/logicselenium/WithBugsPage.java
@@ -6,12 +6,16 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+//import org.openqa.selenium.support.ui.ExpectedConditions;
+//import org.openqa.selenium.support.ui.WebDriverWait;
+import org.openqa.selenium.Keys;
 
 public class WithBugsPage {
 	public static void main(String[] args) throws InterruptedException {
-		
+
 		// Opening the Chrome browser
 		WebDriver driver = new ChromeDriver();
+
 		// Entering the URL
 		driver.get("https://with-bugs.practicesoftwaretesting.com/");
 		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(15));
@@ -22,61 +26,63 @@ public class WithBugsPage {
 		// Finding "RegisteryourAccount" element
 		WebElement registerEle = driver.findElement(By.xpath("//a[text()='Register your account']"));
 		// Selecting the Register your account
-		//Thread.sleep(5000);----->we cant use this method as it waits till the time completes
-		
-		//Clicking on register your account button
-		
+		// Thread.sleep(5000);----->we cant use this method as it waits till the time
+		// completes
+
+		// Clicking on register your account button
+
 		registerEle.click();
-		//Finfing firstname web element
+		// Finfing firstname web element
 		WebElement firstNameText = driver.findElement(By.xpath("//input[@id='first_name']"));
-		//Sending data to first name
+		// Sending data to first name
 		firstNameText.sendKeys("Priyanka");
-		//finding lastname element
+		// finding lastname element
 		WebElement lastNameText = driver.findElement(By.xpath("//input[@id='last_name']"));
-		//Sending last name data
+		// Sending last name data
 		lastNameText.sendKeys("Somapalli");
-		//finding dob element
+		// finding dob element
 		WebElement dateOfBirthEle = driver.findElement(By.xpath("//input[@id='dob']"));
-		//sending dob value
+		// sending dob value
 		dateOfBirthEle.sendKeys("1994-06-16");
-		//finding address
-		WebElement addressText=driver.findElement(By.xpath("//input[@id='address']"));
-		//Sending address
+		// finding address
+		WebElement addressText = driver.findElement(By.xpath("//input[@id='address']"));
+		// Sending address
 		addressText.sendKeys("2021");
-		//Thread.sleep(5000);
-		//finding postalcode element
-		WebElement postalCodeText=driver.findElement(By.xpath("//input[@id='postcode']"));
-		//sending postal value
+		// Thread.sleep(5000);
+		// finding postalcode element
+		WebElement postalCodeText = driver.findElement(By.xpath("//input[@id='postcode']"));
+		// sending postal value
 		postalCodeText.sendKeys("28213");
-		//finding city
-		WebElement cityText=driver.findElement(By.xpath("//input[@id='city']"));
-		//sending city data
+		// finding city
+		WebElement cityText = driver.findElement(By.xpath("//input[@id='city']"));
+		// sending city data
 		cityText.sendKeys("Charlotte");
-		//finding country element
-		WebElement countryElement=driver.findElement(By.xpath("//input[@id='state']"));
-		//Sending country data
+		// finding country element
+		WebElement countryElement = driver.findElement(By.xpath("//input[@id='state']"));
+		// Sending country data
 		countryElement.sendKeys("NC");
-		//finding country element
-		WebElement countryEle=driver.findElement(By.xpath("//select[@id='country']"));
-		//selecting country element
+		// finding country element
+		WebElement countryEle = driver.findElement(By.xpath("//select[@id='country']"));
+		// selecting country element
 		countryEle.sendKeys("USA");
-		//finding phone element
-		WebElement phoneTextEle=driver.findElement(By.xpath("//input[@id='phone']"));
-		//sending phone num
+		// finding phone element
+		WebElement phoneTextEle = driver.findElement(By.xpath("//input[@id='phone']"));
+		// sending phone num
 		phoneTextEle.sendKeys("123456789");
-		//finding email element
-		WebElement emailIdText=driver.findElement(By.xpath("//input[@id='email']"));
-		//sending email
+		// finding email element
+		WebElement emailIdText = driver.findElement(By.xpath("//input[@id='email']"));
+		// sending email
 		emailIdText.sendKeys("priyanka.mandava1412@gmail.com");
-		//finding password element
-		WebElement passwordTextEle=driver.findElement(By.xpath("//input[@id='password']"));
-		//sending password
+		// finding password element
+		WebElement passwordTextEle = driver.findElement(By.xpath("//input[@id='password']"));
+		// sending password
 		passwordTextEle.sendKeys("password123");
+		//Sending tab key
+		passwordTextEle.sendKeys(Keys.TAB);
+
+		WebElement registerButton = driver.findElement(By.xpath("//div/button[text()='Register ']"));
 		
-		//Finding register button
-		WebElement registerButton=driver.findElement(By.xpath("//button[@data-test='register-submit']"));
-		//Clicking on register button
-		//Thread.sleep(8000);
+		Thread.sleep(3000);
 		registerButton.click();
-		}
+	}
 }
--- src/test/java/testNG/MyRegisterTests.java
+++ src/test/java/testNG/MyRegisterTests.java
@@ -1,29 +1,73 @@
 package testNG;
 
+import java.text.SimpleDateFormat;
 import java.time.Duration;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.Test;
 
-public class MyRegisterTests {
-	@Test //Called Annotations
-	public void myRegisterTest()
-	{
-		//open browser
-		WebDriver driver=new ChromeDriver();
-		//using implicit wait
-		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
-		//Sending URL
-		driver.get("https://with-bugs.practicesoftwaretesting.com/#/");
-		//Finding Signin element
-		WebElement signinEle=driver.findElement(By.xpath("//a[text()='Sign in']"));
-		//Click signin
-		signinEle.click();
-		
-		
-	}
+import com.github.javafaker.Faker;
 
+public class MyRegisterTests {
+	@Test // Called Annotations
+	public void myRegisterTest() throws InterruptedException {
+		// open browser
+		WebDriver driver = new ChromeDriver();
+		// using implicit wait
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(15));
+		driver.manage().window().maximize();
+		// Sending URL
+		driver.get("https://with-bugs.practicesoftwaretesting.com/#/");
+		Faker fake = new Faker();
+		SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
+		String DobEle = sdf.format(fake.date().birthday());
+		// Finding Signin element
+		WebElement signinEle = driver.findElement(By.xpath("//a[text()='Sign in']"));
+		// Click signin
+		signinEle.click();
+		WebElement registerEle = driver.findElement(By.xpath("//a[text()='Register your account']"));
+		// Click register
+		registerEle.click();
+		// Finding firstname element
+		WebElement firstNameEle = driver.findElement(By.xpath("//input[@id='first_name']"));
+		// Sending fake first name
+		firstNameEle.sendKeys(new Faker().name().firstName());
+		// finding last name
+		WebElement lastNameEle = driver.findElement(By.xpath("//input[@id='last_name']"));
+		// sending last name fake data
+		lastNameEle.sendKeys(new Faker().name().lastName());
+		// finding address field
+		WebElement addressTextEle = driver.findElement(By.xpath("//input[@id='address']"));
+		WebElement dob = driver.findElement(By.xpath("//input[@id='dob']"));
+		dob.sendKeys(DobEle);
+		// sending fake address
+		addressTextEle.sendKeys(new Faker().address().fullAddress());
+		// finding postal code
+		WebElement postalCodeText = driver.findElement(By.xpath("//input[@id='postcode']"));
+		// sending fake postal code
+		postalCodeText.sendKeys(new Faker().address().zipCode());
+		// finding phone number element
+		WebElement phoneTextEle = driver.findElement(By.xpath("//input[@id='phone']"));
+		// sending fake number
+		phoneTextEle.sendKeys(new Faker().phoneNumber().cellPhone());
+		// finding email element
+		WebElement emailIdText = driver.findElement(By.xpath("//input[@id='email']"));
+		// sending email using faker
+		emailIdText.sendKeys(new Faker().internet().emailAddress());
+		// finding password element
+		WebElement passwordTextEle = driver.findElement(By.xpath("//input[@id='password']"));
+		// sending password
+		passwordTextEle.sendKeys(new Faker().internet().password());
+		passwordTextEle.sendKeys(Keys.TAB);
+		// finding register button
+		WebElement registerButtonEle = driver.findElement(By.xpath("//div/button[text()='Register ']"));
+		// Clicking on register button
+		Thread.sleep(3000);
+		registerButtonEle.click();
+
+	}
 }
