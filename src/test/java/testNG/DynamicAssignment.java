055496732ea2ebabd1769f178471643ea5e26291
priyankasomapalli <priyanka.mandava1412@gmail.com>
2026-03-23 15:43:58 -0400

    Assignment of dynamic control validation by explicit wait

---
+++ src/test/java/testNG/DynamicAssignment.java
@@ -0,0 +1,50 @@
+package testNG;
+
+import java.time.Duration;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class DynamicAssignment {
+	 @Test
+	    public void validateDc() {
+	        WebDriver driver = new ChromeDriver();
+	        driver.navigate().to("https://the-internet.herokuapp.com/");
+	        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(8));
+
+	        clickElement(By.xpath("//a[text()='Dynamic Controls']"), wait);
+	        clickElement(By.xpath("//input[@label='blah']"),wait);
+	        clickElement(By.xpath("//button[text()='Remove']"),wait);
+	        validateText(By.xpath("//h4[text()='Remove/add']"),"Remove/add",wait);
+	        
+	        validateText(By.xpath("//button[text()='Add']"), "Add", wait);
+	        clickElement(By.xpath("//button[text()='Add']"),wait);
+	        WebElement add =driver.findElement(By.xpath("//button[text()='Add']"));
+
+			Assert.assertEquals("//button[text()='Add']", add);
+
+			wait.until(ExpectedConditions.elementToBeClickable(add));
+
+			validateText(By.xpath("//p[@id='message']"), "It's gone!",wait);    
+	        
+	    }
+
+	    public void clickElement(By xpath, WebDriverWait wait) {
+	        WebElement element = wait.until(ExpectedConditions.elementToBeClickable(xpath));
+	        element.click();
+	    }
+	    public void validateText(By xpath, String expectedText, WebDriverWait wait) {
+	        String actualText = wait
+	                .until(ExpectedConditions.visibilityOfElementLocated(xpath))
+	                .getText();
+
+	        Assert.assertEquals(actualText, expectedText);
+	    }
+}
+
--- src/test/java/testNG/DynamicControlsValidation.java
+++ src/test/java/testNG/DynamicControlsValidation.java
@@ -13,29 +13,35 @@ import org.testng.annotations.Test;
 
 public class DynamicControlsValidation {
 	@Test
-	public void validateDc()
-	{
-		WebDriver driver=new ChromeDriver();
-		
-		driver.navigate().to("https://the-internet.herokuapp.com/");
-		
-		WebElement dc=driver.findElement(By.xpath("//a[text()='Dynamic Controls']"));
-		
-		dc.click();
-		
-		WebElement removeButton=driver.findElement(By.xpath("//button[text()='Remove']"));
-		
-		WebDriverWait wait=new WebDriverWait(driver,Duration.ofSeconds(8));
-		
-		wait.until(ExpectedConditions.elementToBeClickable(removeButton));
-		
-	WebElement checkBox	=wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//input[@label='blah']")));
-	checkBox.click();
-	removeButton.click();
-	//Assert.assertEquals(, "//p[@id='message']");
-	
-		
-		
-	}
+	public void validateDc() {
+		WebDriver driver = new ChromeDriver();
 
+		driver.navigate().to("https://the-internet.herokuapp.com/");
+
+		WebElement dc = driver.findElement(By.xpath("//a[text()='Dynamic Controls']"));
+
+		dc.click();
+
+		WebElement removeButton = driver.findElement(By.xpath("//button[text()='Remove']"));
+
+		WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(8));
+
+		wait.until(ExpectedConditions.elementToBeClickable(removeButton));
+
+		WebElement checkBox = wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//input[@label='blah']")));
+
+		checkBox.click();
+
+		removeButton.click();
+
+		WebElement add = driver.findElement(By.xpath("//button[text()='Add']"));
+
+		Assert.assertEquals("//button[text()='Add']", add);
+
+		wait.until(ExpectedConditions.elementToBeClickable(add));
+
+		Assert.assertEquals("//p[@id='message']", "It's gone!");
+
+	}
+	
 }
