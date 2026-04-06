b7f9771af8f46df501be4a5596078e280465a1e3
priyankasomapalli <priyanka.mandava1412@gmail.com>
2026-04-06 00:37:53 -0400

    Assignments on table data

--- src/test/java/testNG/TableTests.java
+++ src/test/java/testNG/TableTests.java
@@ -8,7 +8,20 @@ import org.testng.annotations.Test;
 public class TableTests extends TableBase {
 
 	By javaCheckBox = By.xpath("//fieldset//label/input[@value='Java']");
+
+	By pythonCheckBox = By.xpath("//fieldset//label/input[@value='Python']");
+
 	By languageColumn = By.xpath("//table/tbody/tr/td[3]");
+
+	By languagePython = By.xpath("//table/tbody/tr/td[3][text()='Python']");
+
+	By beginnerCheckBox = By.xpath("//label/input[@value='Beginner']");
+
+	By advancedCheckBox = By.xpath("//label/input[@value='Advanced']");
+
+	By intermediateEle = By.xpath("//table/tbody/tr/td[4][text()='Intermediate']");
+
+	By noDataEle = By.xpath("//div[@id='noData']");
 
 	@Test
 	public void verifyLanguageFieldTest() {
@@ -18,13 +31,47 @@ public class TableTests extends TableBase {
 
 		String language = "Java";
 
-		WebElement languageColumnElements = driver.findElement(languageColumn);
-
-		String textOfLanguageColumn = languageColumnElements.getText();
+		String textOfLanguageColumn = driver.findElement(languageColumn).getText();
 
 		Assert.assertEquals(textOfLanguageColumn, language);
 
 		System.out.println("Only java courses are displayed");
+
+		driver.findElement(pythonCheckBox).click();
+
+		String pythonText = driver.findElement(languagePython).getText();
+
+		Assert.assertEquals(pythonText, "Python");
+
+		System.out.println("Only Python courses are displayed");
+
+	}
+
+	@Test
+	public void levelCheckTest() {
+
+		driver.findElement(beginnerCheckBox).click();
+
+		driver.findElement(advancedCheckBox).click();
+
+		String textOfLevelColumn = driver.findElement(intermediateEle).getText();
+
+		Assert.assertEquals(textOfLevelColumn, "Intermediate");
+
+		System.out.println("Only Intermediate Level elements are displayed");
+
+	}
+
+	@Test
+
+	public void verifyNoMatch() {
+		driver.findElement(pythonCheckBox).click();
+
+		driver.findElement(beginnerCheckBox).click();
+
+		String msg = driver.findElement(noDataEle).getText();
+
+		Assert.assertEquals(msg, "No matching courses.");
 
 	}
 
